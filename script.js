a8b3095cb3398a543d17a97c9ec05b7e1653596b
MiguelD <migueldoliveira142@gmail.com>
2026-06-10 18:35:30 +0100

    Created a loop to create 16 squares inside each row

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 let container = document.querySelector(".container");
 
+// This creates 16 rows
 for(let i = 0; i < 16; i++) {
   let row = document.createElement('div');
   row.className = 'row';
   container.appendChild(row);
+
+
+  // This creates 16 squares inside each row
+  for(let j = 0; j < 16; j++) {
+    let square = document.createElement('div');
+    square.className = 'square';
+    row.appendChild(square);
+  }
 }
